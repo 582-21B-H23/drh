c7a98d30791289cc9fecc2b6e3bde6e32bdb773a
Camille Semaan <csemaan@cmaisonneuve.qc.ca>
2023-03-07 09:33:25 -0500

    Ajout des requêtes plus complexes

---
+++ requetes-select-complexes.sql
@@ -0,0 +1,12 @@
+-- 17) Numéro d'employé, Nom, Date d'embauche, Date fin de poste, et Années de service 
+--      des employés embauchés après 1995, 
+--      triés par années de service en ordre descendant.
+
+
+
+-- 18) Même chose mais seulement pour les employés ayant plus de 25 ans d'années de service
+
+
+-- 19) Même chose mais pour les employés ayant entre 20 et 25 ans de services seulement.
+
+
